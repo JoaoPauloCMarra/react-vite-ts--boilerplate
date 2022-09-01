0aff6a5b15335ecf6471c2b309e2f9627f39d84f
John Marra <joaopaulocmarra@gmail.com>
2022-09-01 13:03:10 -0300

    chore: small improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { StrictMode } from 'react';
+import ErrorBoundary from '~/components/ErrorBoundary';
+
 function App() {
-  return <h1>React Vite TS App</h1>;
+  return (
+    <StrictMode>
+      <ErrorBoundary>
+        <h1>React Vite TS App</h1>
+      </ErrorBoundary>
+    </StrictMode>
+  );
 }
 
 export default App;
